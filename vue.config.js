405ae5fd8a42e62093b18bcb558504f13345bae8
ying486 <851408312@qq.com>
2021-10-05 00:56:31 +0800

    主题定制

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  css: {
+    loaderOptions: {
+      less: {
+        // 定制主题
+        modifyVars: {
+          "primary-color": "#1A4772", // 全局主色
+        },
+        javascriptEnabled: true,
+      },
+    },
+  },
+};
